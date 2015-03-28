b3c5fbb9d9d3b53ea8b97911d1c98d0ad6f390ee
camusbai <camusbai@hotmail.com>
2015-03-28 12:17:06 -0400

    Reverse Integer; Zigzag conversion

--- src/camusbai/leetcode/source/NumericOperation.java
+++ src/camusbai/leetcode/source/NumericOperation.java
@@ -4,7 +4,22 @@ package camusbai.leetcode.source;
  * Created by camusbai on 5/11/2014.
  */
 public class NumericOperation {
-  public int sqrt(int x) {
-    return (int) Math.sqrt(x);
-  }
+    public int sqrt(int x) {
+        return (int) Math.sqrt(x);
+    }
+
+    /*
+    * Reverse Integer
+    * */
+    public static int reverse(int x) {
+        int rvse = 0, bit=0;
+        while (x != 0) {
+            if(++bit==10)
+                if(rvse*10/10!=rvse)
+                    return 0;
+            rvse = rvse * 10 + x % 10;
+            x /= 10;
+        }
+        return x < 0 ? -rvse : rvse;
+    }
 }
