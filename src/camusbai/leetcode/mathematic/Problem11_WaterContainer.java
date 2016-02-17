396d52e5d3b566022c06a6a35e90612874d79cd6
camusbai <camusbai@hotmail.com>
2016-02-16 22:59:08 -0500

    water container recursion stackoverflow

--- src/camusbai/leetcode/mathematic/Problem11_WaterContainer.java
+++ src/camusbai/leetcode/mathematic/Problem11_WaterContainer.java
@@ -7,8 +7,27 @@ package camusbai.leetcode.mathematic;
  * Note: You may not slant the container.
  */
 public class Problem11_WaterContainer {
-  public int maxArea(int[] height) {
+  public static void main(String[] args) {
+    int[] heights = new int[]{2, 3, 4, 5, 5};
 
-    return 0;
+    System.out.println(maxArea(heights));
+  }
+
+  public static int maxArea(int[] height) {
+    if (height.length < 2)
+      return 0;
+    else
+      return getMaxArea(height, height.length);
+  }
+
+  private static int getMaxArea(int[] height, int size) {
+    if (size == 2) {
+      return Math.min(height[0], height[1]);
+    } else {
+      int prevMax = getMaxArea(height, size - 1);
+      for (int i = 1; i < size; i++)
+        prevMax = Math.max(prevMax, i * Math.min(height[size - 1 - i], height[size - 1]));
+      return prevMax;
+    }
   }
 }
